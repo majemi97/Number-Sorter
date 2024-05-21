32c7a4b7deca30a81572e668641dd346b0cb104c
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-21 15:49:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
   event.preventDefault();
-  const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => dropdown.value);
+  const inputValues = [...document.getElementsByClassName("values-dropdown")].map((dropdown) => Number(dropdown.value));
   console.log(inputValues);
 }
 
