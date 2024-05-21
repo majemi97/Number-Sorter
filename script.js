d93d7d018de330f00aa23f2eed00f27819d3d8b5
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-21 16:01:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,143 +1,68 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" type="text/css" href="./styles.css" />
-    <title>Number Sorter</title>
-  </head>
-  <body>
-    <main>
-      <h1>Number Sorter</h1>
-      <form id="form" class="form">
-        <h2>Input:</h2>
-        <fieldset>
-          <span class="bracket">[</span>
-          <div>
-            <select
-              name="values"
-              class="values-dropdown"
-              aria-label="first number"
-            >
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8" selected>8</option>
-              <option value="9">9</option>
-            </select>
-            <span class="comma">,</span>
-          </div>
-          <div>
-            <select
-              name="values"
-              class="values-dropdown"
-              aria-label="second number"
-            >
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2" selected>2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-            <span class="comma">,</span>
-          </div>
-          <div>
-            <select
-              name="values"
-              class="values-dropdown"
-              aria-label="third number"
-            >
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4" selected>4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-            <span class="comma">,</span>
-          </div>
-          <div>
-            <select
-              name="values"
-              class="values-dropdown"
-              aria-label="fourth number"
-            >
-              <option value="0">0</option>
-              <option value="1" selected>1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-            <span class="comma">,</span>
-          </div>
-          <div>
-            <select
-              name="values"
-              class="values-dropdown"
-              aria-label="fifth number"
-            >
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3" selected>3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-          </div>
-          <span class="bracket">]</span>
-        </fieldset>
-        <button id="sort">Sort</button>
-      </form>
-      <div class="output-container">
-        <h2>Output:</h2>
-        <div class="output-array">
-          <span class="bracket">[</span>
-          <div>
-            <span class="output-value" id="output-value-0">0</span>
-            <span class="comma">,</span>
-          </div>
-          <div>
-            <span class="output-value" id="output-value-1">0</span>
-            <span class="comma">,</span>
-          </div>
-          <div>
-            <span class="output-value" id="output-value-2">0</span>
-            <span class="comma">,</span>
-          </div>
-          <div>
-            <span class="output-value" id="output-value-3">0</span>
-            <span class="comma">,</span>
-          </div>
-          <div>
-            <span class="output-value" id="output-value-4">0</span>
-          </div>
-            <span class="bracket">]</span>
-        </div>
-      </div>
-    </main>
-    <script src="./script.js"></script>
-  </body>
-</html>
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown")
+  ].map((dropdown) => Number(dropdown.value));
+
+  const sortedValues = inputValues.sort();
+
+  updateUI(sortedValues);
+}
+
+const updateUI = (array = []) => {
+  array.forEach((num, i) => {
+    const outputValueNode = document.getElementById(`output-value-${i}`);
+    outputValueNode.innerText = num;
+  })
+}
+
+const bubbleSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        const temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
+const selectionSort = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    const temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+
+  return array;
+}
+
+const insertionSort = (array) => {
+  for (let i = 1; i < array.length; i++) {
+    const currValue = array[i];
+    let j = i - 1;
+
+    while (j >= 0 && array[j] > currValue) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currValue;
+  }
+  return array;
+}
+
+sortButton.addEventListener("click", sortInputArray);
