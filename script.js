66dc8170499aef1f07c2601d4a1b288cc1d6fd9c
majemi97 <167489556+majemi97@users.noreply.github.com>
2024-05-21 16:00:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -60,8 +60,9 @@ const insertionSort = (array) => {
       array[j + 1] = array[j];
       j--;
     }
-
+    array[j + 1] = currValue;
   }
+
 }
 
 sortButton.addEventListener("click", sortInputArray);
